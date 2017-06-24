34f304aa725cfa0602b57423a35387ea9f83fd9c
Asyade <zarandifar@gmail.com>
2017-06-24 10:08:44 +0200

    Add files via upload

---
+++ js/camManager.js
@@ -0,0 +1,63 @@
+(function() {
+
+  var streaming = false,
+      video        = document.querySelector('#video'),
+      cover        = document.querySelector('#cover'),
+      canvas       = document.querySelector('#img-result'),
+      photo        = document.querySelector('#photo'),
+      startbutton  = document.querySelector('#take-btn'),
+      width = $( window ).width(),
+      height = $( window ).height()
+
+  navigator.getMedia = ( navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia);
+
+  navigator.getMedia(
+    {
+      video: true,
+      audio: false
+    },
+    function(stream) {
+      if (navigator.mediaDevices.getUserMedia) {
+        video.srcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        video.src = vendorURL.createObjectURL(stream);
+      }
+      video.play();
+    },
+    function(err) {
+      console.log(err);
+    }
+  );
+
+  video.addEventListener('canplay', function(ev){
+    if (!streaming) {
+      height = video.videoHeight / (video.videoWidth/width);
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      scale_video("#video", "#img-result");
+      streaming = true;
+    }
+  }, false);
+
+  function takepicture() {
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+    var data = canvas.toDataURL('image/png');
+   // console.log(data);
+    request_api(data);
+  }
+
+  startbutton.addEventListener('click', function(ev){
+      $("#img-result").slideToggle();
+      $("#take-btn").fadeOut();
+      takepicture();
+    ev.preventDefault();
+  }, false);
+})();
---
+++ js/camUtils.js
@@ -0,0 +1,105 @@
+function dataURLtoBlob(dataurl) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {type:mime});
+}
+
+
+
+function	scale_video(video, res)
+{
+    var height = $(window).height();
+    $(video).css('height', height);
+
+    var videoWidth = $(video).width(),
+        windowWidth = $(window).width(),
+    marginLeftAdjust =   (windowWidth - videoWidth) / 2;
+
+    $(video).css({
+        'height': height, 
+        'marginLeft' : marginLeftAdjust
+    });
+    $(res).css({
+        'marginLeft' : marginLeftAdjust,
+    });
+    if ($(video).css("width") < $(video).css("height"))
+    {
+    	 $(res).css({
+	    	'width' : ($(video).css("width")),
+	    });
+    }
+    else
+    {
+		$(res).css({
+			'height' : ($(video).css("height")),
+		});
+    }
+    $(res).css({
+    	'marginLeft' : marginLeftAdjust,
+    	'margin-top' : ($(window).height() / 2) - ($(res).height() / 2)
+    });
+}
+
+$(window).resize(function(){
+	scale_video("#video", "#img-result");
+});
+
+$(window).load(function(){
+	$( "#img-result" ).slideToggle();
+});
+
+function	request_api(d)
+{
+    var params = {
+    };
+    $.ajax({
+        url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?" + $.param(params),
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","21b9bd9785ef4bf396783ac499ecfb9c");
+        },
+        type: "POST",
+        processData: false,
+		contentType: false,
+        data: dataURLtoBlob(d)
+
+    })
+    .done(function(data) {
+    	if (typeof(data[0]) != "undefined" && typeof(data[1]) == "undefined")
+    	{
+        	console.log(JSON.stringify(data));
+        	post("http://localhost:8080/panel.html", {}, 'post');
+    	}
+        else
+        {
+			$("#img-result").slideToggle();
+			$("#take-btn").fadeIn();
+        }
+    })
+    .fail(function() {
+        console.log("Api error !");
+    });
+}
+
+function post(path, params, method) {
+    method = method || "post";
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+    document.body.appendChild(form);
+    form.submit();
+}
