a66febeaee4c8f9e5a2572ecd64d5cc0f4b0c1a1
Kantum <kentin@ymail.com>
2017-06-25 09:35:22 +0200

    Added some quotes

--- js/ifforest.js
+++ js/ifforest.js
@@ -9,6 +9,7 @@ var			textes = {
 			"What a great form today, you should use that energy to prepare a good meal.",
 			"What a great form today, you may use that energy to do some sport.",
 			"What a dynamism today ! Time to share innovative ideas !",
+			"I'm not perfect but I'm me and thats even better",
 			"Show off this smile like a trophy all day long !",
 			"	- My brother just got a puppy. \
 				- Do you help take care of him ? \
@@ -16,10 +17,19 @@ var			textes = {
 
 			"- Father : Why did you put that frog in your brother's bed ? \
 				- Son : So disgusted !  I couldn't find any worms !"
+			"“Be yourself; everyone else is already taken.” \
+			― Oscar Wilde",
+			"“You've gotta dance like there's nobody watching, \
+			Love like you'll never be hurt, \
+			Sing like there's nobody listening, \
+			And live like it's heaven on earth.” \
+			― William W. Purkey",
 		],
 
 	'neutral' :
 		[
+			"“Happiness is not something ready made. It comes from your own actions.” \
+			― Dalai Lama XIV",
 			"Don't seem very enthousiastic today... Try and say cheese ! That's good ! Now keep smiling and make your day better again.",
 			"Meh Dou you know there is a drone race at 12 in the hall ?",
 			"Time to make a break ! Why not having a coffee with some colleagues ?",
@@ -28,9 +38,19 @@ var			textes = {
 				- The dog got graduated.",
 			"Quick massage : Rub your eyebrows with your thumbs by putting some circular pressure.",
 			"Go and pick up some chocolate to your colleague !"
+			"“It's so hard to forget pain, but it's even harder to remember sweetness. We have no scar to show for happiness. We learn so little from peace.” \
+			― Chuck Palahniuk, Diary",
+			"“Be the change that you wish to see in the world.” \
+			― Mahatma Gandhi",
 		],
 	'sad' :
 		[
+			"“Count your age by friends, not years. Count your life by smiles, not tears.” \
+			― John Lennon",
+			"“For every minute you are angry you lose sixty seconds of happiness.” \
+			― Ralph Waldo Emerson",
+			"“The best way to cheer yourself up is to try to cheer somebody else up.” \
+			Mark Twain",
 			"Hard day ? Why not having a coffee with some colleagues.",
 			"A guy shows up late for work. The boss yells, You should have been here at 8.30! He replies. Why? What happened at 8.30?",
 			"Tips for relaxation : Fill your belly while breathing in, and withdraw it while breathing out.  ",
